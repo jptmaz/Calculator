7b77ca614b0ada8d1db026959a4fc71b97568c74
Jasmin Paige <jaspagmaz98@gmail.com>
2023-05-11 18:13:53 +0800

    Check the entire program and fix bugs

--- calculator_program.py
+++ calculator_program.py
@@ -3,18 +3,19 @@ while True:
     # Ask the user for the operation they want to use
     print("Which mathematical operation would you like to choose? ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION")
     operation = input("= ").upper()
+    
     # Create if, elif, and else function for +
     if operation == "ADDITION":
         print("You chose addition!")
-        # Ask for numbers and create a program for the operation
         try:
+              # Ask for numbers and create a program for the operation
             number1 = input("What is your first number: ")
             number2 = input("What is your second number: ")
             sum = int(number1) + int(number2)
             print(sum)
         except ValueError:
             print("An error has occurred, please try again.")
-            
+
         # Ask the user if they want to repeat the program from the beginning.
         print("Would you like to try again?")
         Try_again = input("= ").upper()
@@ -22,11 +23,11 @@ while True:
             True
         elif Try_again == "NO":
             break
-          
-    # Create if, elif, and else function for -
-     elif operation == "SUBTRACTION":
-         print("You chose subtraction!")
-         # Ask for numbers and create a program for the operation
+
+     # Create if, elif, and else function for -
+    elif operation == "SUBTRACTION":
+        print("You chose subtraction!")
+        # Ask for numbers and create a program for the operation
         try:
             number1 = input("What is your first number: ")
             number2 = input("What is your second number: ")
@@ -34,7 +35,7 @@ while True:
             print(difference)
         except ValueError:
             print("An error has occurred, please try again.")
-        
+
          # Ask the user if they want to repeat the program from the beginning.
         print("Would you like to try again?")
         Try_again = input("= ").upper()
@@ -42,8 +43,8 @@ while True:
             True
         elif Try_again == "NO":
             break
-        
-    # Create if, elif, and else function for *
+
+     # Create if, elif, and else function for *
     elif operation == "MULTIPLICATION":
         print("You chose multiplication!")
         # Ask for numbers and create a program for the operation
@@ -54,14 +55,15 @@ while True:
             print(product)
         except ValueError:
             print("An error has occurred, please try again.")
-        # Ask the user if they want to repeat the program from the beginning.
+
+         # Ask the user if they want to repeat the program from the beginning.
         print("Would you like to try again?")
         Try_again = input("= ").upper()
         if Try_again == "YES":
             True
         elif Try_again == "NO":
             break
-    # Create if, elif, and else function for /
+
     elif operation == "DIVISION":
         print("You chose division!")
         # Ask for numbers and create a program for the operation
@@ -72,8 +74,9 @@ while True:
             print(quotient)
         except ValueError:
             print("An error has occurred, please try again.")
-        # Ask the user if they want to repeat the program from the beginning.
-         print("Would you like to try again?")
+
+         # Ask the user if they want to repeat the program from the beginning.
+        print("Would you like to try again?")
         Try_again = input("= ").upper()
         if Try_again == "YES":
             True
