54a67f10769eb03643573c1e028169660f319d9d
Jasmin Paige <jaspagmaz98@gmail.com>
2023-05-11 17:47:27 +0800

    If statement Addition

--- calculator_program.py
+++ calculator_program.py
@@ -3,7 +3,17 @@ while True:
     # Ask the user for the operation they want to use
     print("Which mathematical operation would you like to choose? ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION")
     operation = input("= ").upper()
-# Create if, elif, and else function for +
+    # Create if, elif, and else function for +
+    if operation == "ADDITION":
+        print("You chose addition!")
+        try:
+            number1 = input("What is your first number: ")
+            number2 = input("What is your second number: ")
+            sum = int(number1) + int(number2)
+            print(sum)
+        except ValueError:
+            print("An error has occurred, please try again.")
+
 # Ask for numbers and create a program for the operation
 # Ask the user if they want to repeat the program from the beginning.
 # Ask for numbers and create a program for the operation
