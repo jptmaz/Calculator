1d5633287cedb9e7f7e8ef2669fe53d9e5f0e74e
Jasmin Paige <jaspagmaz98@gmail.com>
2023-05-11 17:34:42 +0800

    Operation value added

---
+++ calculator_program.py
@@ -0,0 +1,19 @@
+# Create a while function
+while True:
+    # Ask the user for the operation they want to use
+    print("Which mathematical operation would you like to choose? ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION")
+    operation = input("= ").upper()
+# Create if, elif, and else function for +
+# Ask for numbers and create a program for the operation
+# Ask the user if they want to repeat the program from the beginning.
+# Ask for numbers and create a program for the operation
+# Ask the user if they want to repeat the program from the beginning.
+# Create if, elif, and else function for -
+# Ask for numbers and create a program for the operation
+# Ask the user if they want to repeat the program from the beginning.
+# Create if, elif, and else function for *
+# Ask for numbers and create a program for the operation
+# Ask the user if they want to repeat the program from the beginning.
+# Create if, elif, and else function for /
+# Ask for numbers and create a program for the operation
+# Ask the user if they want to repeat the program from the beginning.
